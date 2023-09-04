7075140606c0e43d512832ed9e07061fc2cb32d7
Juan Montero <juanca.montero76@gmail.com>
2023-09-04 16:34:43 -0500

    new branch pull request Vercel test

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Juank Monty";
+const name = "Juank Montero";
 export const siteTitle = "Juank's  sample Website";
 
 export default function Layout({ children, home }) {
